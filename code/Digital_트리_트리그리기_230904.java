467313583ff21f8854ad5dfcdff8b92c0caca1a6
이수진 <whdlsj98@naver.com>
2023-09-04 08:57:29 +0900

    solve tree problem

--- code/Digital_트리_트리그리기_230904.java
+++ code/Digital_트리_트리그리기_230904.java
@@ -1,2 +1,21 @@
+
+import java.util.*;
 public class Digital_트리_트리그리기_230904 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+
+        // 루트 노드에 리프 노드를 붙임
+        for (int i = 1; i <= m; i++) {
+            System.out.println("0 " + i);
+        }
+
+        // 나머지 리프 노드가 아닌 노드를 붙임
+        for (int i = m + 1; i < n; i++) {
+            System.out.println(i - m + " " + i);
+        }
+    }
 }
