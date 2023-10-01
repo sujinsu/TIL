c7943677bc1b5135fde0257588e813b0fabb0e8d
이수진 <whdlsj98@naver.com>
2023-10-01 17:06:45 +0900

    231001

--- code/Pro_완전탐색_소수찾기_230930.java
+++ code/Pro_완전탐색_소수찾기_230930.java
@@ -1,7 +1,13 @@
 import java.util.*;
-public class Pro_완전탐색_소수찾기_230930_ing {
+
+/**
+ * 순열 및 탐색 로직 더 공부하기
+ */
+public class Pro_완전탐색_소수찾기_230930 {
+    boolean[] isPrime;
+
     public int solution(String numbers) {
-        int answer = 0;
+
 
         int[] nums = new int[numbers.length()];
         for (int i = 0; i < numbers.length(); i++) {
@@ -9,8 +15,14 @@ public class Pro_완전탐색_소수찾기_230930_ing {
         }
 
         List<Integer> allCombinationsAndPermutations = generateAll(nums);
+
+        int max = Collections.max(allCombinationsAndPermutations);
+        isPrime(max);
+        int answer = 0;
         for (Integer combo : allCombinationsAndPermutations) {
-            System.out.println(combo);
+            if(isPrime[combo]){
+                answer +=1;
+            }
         }
 
 
@@ -41,6 +53,23 @@ public class Pro_완전탐색_소수찾기_230930_ing {
                         remaining.substring(0, i) + remaining.substring(i + 1),
                         result
                 );
+            }
+        }
+    }
+
+    // 소수를 판별하는 함수
+    void isPrime(int num){
+        isPrime =  new boolean[num+1];
+
+        for(int i=2;i<=num;i++){
+            isPrime[i] = true;
+        }
+
+        for(int i=2;i * i<=num;i++){
+            if(isPrime[i]){
+                for(int j=i * i; j <=num;j+=i){
+                    isPrime[j] = false;
+                }
             }
         }
     }
