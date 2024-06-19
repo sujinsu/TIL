b68a471c8755433b338a9fb3ee3e3654ad0c2345
이수진 <whdlsj98@naver.com>
2024-06-20 08:59:02 +0900

    builder패턴 240620

---
+++ code/GoF/src/main/java/com/example/creational_pattern/builder/after/App.java
@@ -0,0 +1,9 @@
+package com.example.creational_pattern.builder.after;
+
+public class App {
+
+    public static void main(String[] args) {
+        TourPlanBuilder builder = new DefaultTourBuilder();
+        
+    }
+}
---
+++ code/GoF/src/main/java/com/example/creational_pattern/builder/after/TourPlanBuilder.java
@@ -0,0 +1,27 @@
+package com.example.creational_pattern.builder.after;
+
+import com.example.creational_pattern.builder.before.TourPlan;
+
+import java.time.LocalDate;
+
+/**
+ * TourPlanBuilder 을 return type -> title 메서드 호출 후 응답 받은 빌더로 또 다른 메서드 호출 가능
+ */
+public interface TourPlanBuilder {
+
+    TourPlanBuilder title(String title);
+
+    TourPlanBuilder nightsAndDays(int nights, int days);
+
+    TourPlanBuilder startDate(LocalDate localDate);
+
+    TourPlanBuilder whereToStay(String whereToStay);
+
+    TourPlanBuilder addPlan(int day, String plan);
+
+    /**
+     * 데이터 검증 가능 (ex. nights는 세팅하고 days는 세팅 안됨, 장거리인데 stay 값이 없진 않은지)
+     * @return
+     */
+    TourPlan getPlan();
+}
