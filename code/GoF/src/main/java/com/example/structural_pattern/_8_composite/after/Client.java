7b3d8be8ad2bb6cd50cd404a79c0aad0f4192d3e
이수진 <whdlsj98@naver.com>
2024-07-09 08:55:27 +0900

    composit pattern java

--- code/GoF/src/main/java/com/example/structural_pattern/_8_composite/after/Client.java
+++ code/GoF/src/main/java/com/example/structural_pattern/_8_composite/after/Client.java
@@ -1,6 +1,14 @@
 package com.example.structural_pattern._8_composite.after;
 
 
+/**
+ * composit pattern에서 client는 구체적인 정보를 알 필요 없음.
+ * component라는 인터페이스가 알고 있음
+ *
+ * (+) : 복잡한 트리 구조를 편리하게 사용 / 다형성과 재귀 활용 / 클라이언트 코드 변경 x 새로운 element type 추가 가능
+ * (-) : 트리를 만들어야 함(공통된 인터페이스 정의) 지나친 일반화를 해야 하는 상황
+ *  ㄴ ex) 런타임에 타입체크가 필요한 상황 등 (백이냐 아이템이냐 ) 에서는 꼭 디자인 패턴을 적용 시켜야 하는 상황인지 재고
+ */
 public class Client {
 
     public static void main(String[] args) {
--- code/GoF/src/main/java/com/example/structural_pattern/_8_composite/java/SwingExample.java
+++ code/GoF/src/main/java/com/example/structural_pattern/_8_composite/java/SwingExample.java
@@ -4,10 +4,16 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
+/**
+ * 서로 다른 노드, primitive type 과 그걸 감싸는 wrapper type 이 동일한 인터페이스 구현한 경우
+ *
+ * - Java swing :  JFrame(컴포짓 객체), component (component interface)
+ * - Java server 라이브러리
+ */
 public class SwingExample {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame(); // JFrame : TextField, JButton 추가 가능 (프레임이란 공간에 텍스트, 버튼 추가 하는 것)
 
         JTextField textField = new JTextField();
         textField.setBounds(200, 200, 200, 40);
@@ -20,7 +26,9 @@ public class SwingExample {
 
         frame.setSize(600, 400);
         frame.setLayout(null);
-        frame.setVisible(true);
+        frame.setVisible(true); // JFrame, JTextField, JButton 상위상위 ~~ 클래스 컴포넌트에서 만남
+        // component가 가진 setVisible을 하면 그 안의 모든 element도 보여줌
+        //  JTextField, JButton (leaf)
     }
 
 
