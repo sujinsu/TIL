c534087ca5cdf06d2cd78cbb55a1489d47b6d840
이수진 <whdlsj98@naver.com>
2024-07-19 08:57:29 +0900

    command pattern code update

---
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_14_command/after/Command.java
@@ -0,0 +1,9 @@
+package com.example.behavioral_patterns._14_command.after;
+
+public interface Command {
+
+    void execute();
+
+    void undo();
+
+}
---
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_14_command/package-info.java
@@ -0,0 +1,1 @@
+package com.example.behavioral_patterns._14_command;
