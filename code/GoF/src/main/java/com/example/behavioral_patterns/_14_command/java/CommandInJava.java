dbcae426cb1a9369cfe6a2ee4698d511c0f44a8a
이수진 <whdlsj98@naver.com>
2024-07-23 08:56:33 +0900

    command pattern code java

--- code/GoF/src/main/java/com/example/behavioral_patterns/_14_command/java/CommandInJava.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_14_command/java/CommandInJava.java
@@ -7,13 +7,32 @@ import com.example.behavioral_patterns._14_command.before.Light;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 각각의 커맨드를 executorService 가 실행시켜 줌
+ */
 public class CommandInJava {
 
     public static void main(String[] args) {
         Light light = new Light();
         Game game = new Game();
+        // Executors : threadpool 만들어주는 factory같은 클래스
         ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        // 메서드 레퍼런스 (Java8 도입)
+        // 4개의 thread를 가진 threadpool을 만들어 준 것 >> 비동기적으로 오퍼레이션 실행 기능 제공
         executorService.submit(light::on);
+
+        // 익명 내부 클래스 : 기존 Java 방식
+        // 아래와 같이 runnable이란 구현체를 준 것과 비슷한 효과 >> runnable 타입 인스턴스에 해야할 일 모두 들어있다 >> 이 자체가 커맨드, 커맨트 패턴 적용
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                light.on();
+            }
+        });
+        // 람다 표현식 : 함수형 프로그래밍 선호시 사용
+        executorService.submit(()-> light.on());
+
         executorService.submit(game::start);
         executorService.submit(game::end);
         executorService.submit(light::off);
--- code/GoF/src/main/java/com/example/behavioral_patterns/_14_command/java/CommandInSpring.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_14_command/java/CommandInSpring.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Simple JDBC Insert : JDBC template을 내부적으로 사용
+ */
 public class CommandInSpring {
 
     private DataSource dataSource;
@@ -17,6 +20,7 @@ public class CommandInSpring {
     }
 
     public void add(Command command) {
+        // SimpleJdbcInsert : insert query를 실행하는 데 필요한 모든 정보를 가지고 있는 하나의 커맨드 오브젝트
         SimpleJdbcInsert insert = new SimpleJdbcInsert(dataSource)
                 .withTableName("command")
                 .usingGeneratedKeyColumns("id");
@@ -25,6 +29,9 @@ public class CommandInSpring {
         data.put("name", command.getClass().getSimpleName());
         data.put("when", LocalDateTime.now());
         insert.execute(data);
+
+
+        // JDBC call : 비슷한 예시 >> stored procedure 호출 시 사용할 수 있는 커맨드 인스턴스를 만들 수 있는 편리한 클래스
     }
 
 }
