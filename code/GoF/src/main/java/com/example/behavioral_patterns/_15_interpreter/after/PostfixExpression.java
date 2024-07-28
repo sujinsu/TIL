fa2877f9545080fbc3e141d1d42858b76be580b0
이수진 <whdlsj98@naver.com>
2024-07-29 08:54:49 +0900

    interpreter pattern after

--- code/GoF/src/main/java/com/example/behavioral_patterns/_15_interpreter/after/PostfixExpression.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_15_interpreter/after/PostfixExpression.java
@@ -6,4 +6,25 @@ public interface PostfixExpression {
 
     int interpret(Map<Character, Integer> context);
 
+    static PostfixExpression plus(PostfixExpression left, PostfixExpression right){
+        return new PostfixExpression() {
+            @Override
+            public int interpret(Map<Character, Integer> context) {
+                return left.interpret(context) + right.interpret(context);
+            }
+        };
+        // 람다로 표현 가능
+//        return context -> left.interpret(context) + right.interpret(context);
+    }
+
+    static PostfixExpression minus(PostfixExpression left, PostfixExpression right){
+        // 람다로 표현 가능
+        return context -> left.interpret(context) - right.interpret(context);
+    }
+
+    static PostfixExpression variable(Character c){
+        return context -> context.get(c);
+        // 람다로 표현 가능
+//        return context -> left.interpret(context) + right.interpret(context);
+    }
 }
--- code/GoF/src/main/java/com/example/behavioral_patterns/_15_interpreter/after/PostfixParser.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_15_interpreter/after/PostfixParser.java
@@ -2,11 +2,16 @@ package com.example.behavioral_patterns._15_interpreter.after;
 
 import java.util.Stack;
 
+import static com.example.behavioral_patterns._15_interpreter.after.PostfixExpression.minus;
+import static com.example.behavioral_patterns._15_interpreter.after.PostfixExpression.plus;
+import static com.example.behavioral_patterns._15_interpreter.after.PostfixExpression.variable;
+
 /**
  * 문자가 들어오면 캐릭터를 순회하면서 파싱
  */
 public class PostfixParser {
 
+    //TODO xyz+-
     public static PostfixExpression parse(String expression) {
         // expression들의 스택
         Stack<PostfixExpression> stack = new Stack<>();
@@ -19,13 +24,16 @@ public class PostfixParser {
     private static PostfixExpression getExpression(char c, Stack<PostfixExpression> stack) {
         switch (c) {
             case '+':
-                return new PlusExpression(stack.pop(), stack.pop());
+//                return new PlusExpression(stack.pop(), stack.pop());
+                return plus(stack.pop(), stack.pop());
             case '-':
                 PostfixExpression right = stack.pop();
                 PostfixExpression left = stack.pop();
-                return new MinusExpression(left, right);
+//                return new MinusExpression(left, right);
+                return minus(left, right);
             default:
-                return new VariableExpression(c);
+//                return new VariableExpression(c);
+                return variable(c);
         }
     }
 }
