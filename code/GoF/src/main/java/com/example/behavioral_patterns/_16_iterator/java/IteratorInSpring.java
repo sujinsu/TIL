4171bfd650ed3a5b41236e79f31e84b7441a821d
이수진 <whdlsj98@naver.com>
2024-07-30 08:45:17 +0900

    iterator pattern code

---
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_16_iterator/java/IteratorInSpring.java
@@ -0,0 +1,10 @@
+package com.example.behavioral_patterns._16_iterator.java;
+
+import org.springframework.util.CompositeIterator;
+
+public class IteratorInSpring {
+
+    public static void main(String[] args) {
+        CompositeIterator iterator;
+    }
+}
---
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_16_iterator/package-info.java
@@ -0,0 +1,1 @@
+package com.example.behavioral_patterns._16_iterator;
