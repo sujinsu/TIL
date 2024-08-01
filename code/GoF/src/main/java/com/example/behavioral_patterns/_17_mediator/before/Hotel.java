d8e85bca5a9dd6585c12f96ed23e7727ae8aa9cb
이수진 <whdlsj98@naver.com>
2024-08-02 08:54:01 +0900

    mediator pattern  java

--- code/GoF/src/main/java/com/example/behavioral_patterns/_17_mediator/before/Hotel.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_17_mediator/before/Hotel.java
@@ -2,10 +2,15 @@ package com.example.behavioral_patterns._17_mediator.before;
 
 /**
  * mediator pattern : 여러 객체 간의 의사소통 방법을 추상화 시켜서 여러 객체 간의 의존성 결합도를 낮추는 방법
+ * ㄴ 여러 객체들이 소통하는 방법을 캡슐화하는 패턴
  * 
  * 실생활 예제 >> 층간 소음 등 서로 직접 방문 x, 관리 사무소 & 비행 상황 시 관제탑
  *
  * colleague는 colleague를 참조하지 않음. mediator를 참조.
+ *
+ * (+) : 컴포넌트 코드를 변경하지 않고 새로운 중재자를 만들어 사용
+ * (+) : 각각의 컴포넌트 코드를 보다 간결하게 유지
+ * (-) : 중재자 역할 클래스의 복잡도와 결합도 증가
  */
 public class Hotel {
 
--- code/GoF/src/main/java/com/example/behavioral_patterns/_17_mediator/java/MediatorInSpring.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_17_mediator/java/MediatorInSpring.java
@@ -2,6 +2,12 @@ package com.example.behavioral_patterns._17_mediator.java;
 
 import org.springframework.web.servlet.DispatcherServlet;
 
+/**
+ * 중재자 패턴 java, spring 예제
+ *
+ * - java : ExecutorService, Executor
+ * - spring : DispatcherServlet >>  여러 resolver, adapter, handler 등등 컴포넌트 간 중재자 역할을 함. 다만 DispatcherServlet 에서 단방향으로 아는 관계
+ */
 public class MediatorInSpring {
 
     public static void main(String[] args) {
