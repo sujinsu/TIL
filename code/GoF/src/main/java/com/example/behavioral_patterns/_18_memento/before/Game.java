ea5c375ef9ff603e49692ed4bc41d7567ebdb940
이수진 <whdlsj98@naver.com>
2024-08-06 08:31:08 +0900

    memento pattern  code

---
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_18_memento/before/Game.java
@@ -0,0 +1,26 @@
+package com.example.behavioral_patterns._18_memento.before;
+
+import java.io.Serializable;
+
+public class Game implements Serializable {
+
+    private int redTeamScore;
+
+    private int blueTeamScore;
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+}
---
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_18_memento/package-info.java
@@ -0,0 +1,1 @@
+package com.example.behavioral_patterns._18_memento;
