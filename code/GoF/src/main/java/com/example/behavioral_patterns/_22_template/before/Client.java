4959d7bb70622f483264d92964c16c15970f6d9b
이수진 <whdlsj98@naver.com>
2024-08-14 08:52:38 +0900

    template pattern before

--- code/GoF/src/main/java/com/example/behavioral_patterns/_22_template/before/Client.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_22_template/before/Client.java
@@ -1,5 +1,12 @@
 package com.example.behavioral_patterns._22_template.before;
 
+/**
+ * Template pattern : 알고리즘 구조를 서브 클래스가 확장할 수 있도록 템플릿으로 제공
+ *  ㄴ 만약 지금의 더하는 과정 외에 곱하는 것이 필요하다면 파일읽어오는 부분 등 중복 발생 ! >> template 필요
+ *
+ * - AbstractClass 하나
+ * - ConcreteClass 하나 or 여러개
+ */
 public class Client {
 
     public static void main(String[] args) {
--- code/GoF/src/main/java/com/example/behavioral_patterns/_22_template/before/FileProcessor.java
+++ code/GoF/src/main/java/com/example/behavioral_patterns/_22_template/before/FileProcessor.java
@@ -12,10 +12,13 @@ public class FileProcessor {
     }
 
     public int process() {
+        // try resource : java8부터 제공 >> try문이 끝날 때 리소스를 닫아주는 메서드를 자동으로 호출
         try(BufferedReader reader = new BufferedReader(new FileReader(path))) {
             int result = 0;
             String line = null;
             while((line = reader.readLine()) != null) {
+                // Integer.parseInt >> int 반환
+                // ValueOf >> Integer 반환
                 result += Integer.parseInt(line);
             }
             return result;
